cabffb1d173350e2c7727dccca2838aaaee75e3b
Pallies <palliescie@gmail.com>
2022-12-01 11:02:34 +0100

    add article

--- articles-db/bdd/articles-bdd.sql
+++ articles-db/bdd/articles-bdd.sql
@@ -10,3 +10,5 @@ insert into articles(libelle,prix) values ("cerise",4.45);
 insert into articles(libelle,prix) values ("tomate",3.15);
 insert into articles(libelle,prix) values ("salade",0.95);
 insert into articles(libelle,prix) values ("carotte",1.45);
+
+insert into articles(libelle,prix) values ("pomme de terre",5.03);
