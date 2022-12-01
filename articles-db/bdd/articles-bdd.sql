6bada80090724a2f0c23697b07c6fc2d2233dc43
Pallies <palliescie@gmail.com>
2022-12-01 10:59:09 +0100

    init bdd and image-bdd with volume

---
+++ articles-db/bdd/articles-bdd.sql
@@ -0,0 +1,12 @@
+create table articles (
+    id int not null auto_increment,
+    libelle varchar(50) not null,
+    prix float not null,
+    primary key (id)
+);
+
+insert into articles(libelle,prix) values ("melon",1.15);
+insert into articles(libelle,prix) values ("cerise",4.45);
+insert into articles(libelle,prix) values ("tomate",3.15);
+insert into articles(libelle,prix) values ("salade",0.95);
+insert into articles(libelle,prix) values ("carotte",1.45);
